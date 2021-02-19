6a65d982e84e08f6370c7f1891b2ce8c96f7fe12
Andreas Velounias <>
2021-02-19 17:08:26 +0000

    Add dummy data

--- app/lib/category_meals_screen.dart
+++ app/lib/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:app/cateory_item.dart';
 import 'package:app/models/category.dart';
 import 'package:flutter/material.dart';
+import 'dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -8,13 +9,18 @@ class CategoryMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(routeArgs['id']);
+    }).toList();
     return Scaffold(
-      appBar: AppBar(
-        title: Text(routeArgs['title']),
-      ),
-      body: Center(
-        child: Text('Recipes'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(routeArgs['title']),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Text(categoryMeals[index].title);
+          },
+          itemCount: categoryMeals.length,
+        ));
   }
 }
